1bf25367f24e769fc9a86edd129af01e05427b0d
KAMATE KATENDE <kamatekatende@gmail.com>
2025-11-26 17:41:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-995755513-d4874",
+  "appId": "1:538107193594:web:15613a0c14c49ec64ac143",
+  "apiKey": "AIzaSyBkdpyPNR9-au2QFFzBZPOuFv7xCagiWtc",
+  "authDomain": "studio-995755513-d4874.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "538107193594"
+};
